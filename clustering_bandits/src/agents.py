06307a81d4fcbc4abb04d9f56b10fccf3e625388
fulcus <francescofgonzales@gmail.com>
2023-02-06 18:59:08 +0100

    removed agent.reset()

--- clustering_bandits/src/agents.py
+++ clustering_bandits/src/agents.py
@@ -10,7 +10,10 @@ class Agent(ABC):
         self.n_contexts = context_set.shape[0]
         self.n_arms = arms.shape[0]
         self.arm_dim = arms.shape[1]
-        self.reset()
+        self.t = 0
+        self.a_hist = []
+        self.last_pull_i = None
+        self.last_pulls_i = np.zeros(self.n_contexts, dtype=int)
 
     @abstractmethod
     def pull_arm(self, context_i):
@@ -26,24 +29,19 @@ class Agent(ABC):
         pass
 
     def update_arms(self, rewards):
-        for arm_i, reward in zip(self.last_pulls_i, rewards):
-            self.update(reward, arm_i=arm_i)
-
-    def reset(self):
-        self.t = 0
-        self.a_hist = []
-        self.last_pull_i = None
-        self.last_pulls_i = np.zeros(self.n_contexts, dtype=int)
+        for arm_i, reward, c_i in zip(self.last_pulls_i, rewards, range(self.n_contexts)):
+            self.update(reward, arm_i=arm_i, context_i=c_i)
+        self.t += 1
 
 
 class Clairvoyant(Agent):
     """Always pulls the optimal arm"""
 
     def __init__(self, arms, context_set, theta, psi, theta_p):
+        super().__init__(arms, context_set)
         self.theta = theta
         self.theta_p = theta_p
         self.psi = psi
-        super().__init__(arms, context_set)
 
     def pull_arm(self, context_i):
         exp_rewards = np.zeros(self.n_arms)
@@ -56,22 +54,17 @@ class Clairvoyant(Agent):
         self.a_hist.append(self.last_pull_i)
         return self.last_pull_i
 
-    def update(self, reward, arm_i=None):
+    def update(self, reward, arm_i=None, context_i=None):
         if arm_i is not None:
             self.last_pull_i = arm_i
-        self.t += 1
 
 
 class UCB1Agent(Agent):
     def __init__(self, arms, context_set, max_reward=1):
-        self.max_reward = max_reward
         super().__init__(arms, context_set)
-
-    def reset(self):
-        super().reset()
+        self.max_reward = max_reward
         self.avg_reward = np.zeros(self.n_arms)
         self.n_pulls = np.zeros(self.n_arms)
-        return self
 
     def pull_arm(self, context_i=None):
         ucb1 = [self.avg_reward[a] + self.max_reward *
@@ -82,40 +75,36 @@ class UCB1Agent(Agent):
         self.a_hist.append(self.last_pull_i)
         return self.last_pull_i
 
-    def update(self, reward, arm_i=None):
+    def update(self, reward, arm_i=None, context_i=None):
         if arm_i is not None:
             self.last_pull_i = arm_i
         self.avg_reward[arm_i] = ((
             self.avg_reward[arm_i]
             * self.n_pulls[arm_i] + reward)
             / (self.n_pulls[arm_i] + 1))
-        self.t += 1
 
 
 class LinUCBAgent(Agent):
     def __init__(self, arms, context_set, horizon, lmbd,
                  max_theta_norm_sum, max_arm_norm, sigma=1):
+        super().__init__(arms, context_set)
         assert lmbd > 0
         self.lmbd = lmbd
         self.horizon = horizon
         self.max_theta_norm_sum = max_theta_norm_sum
         self.max_arm_norm = max_arm_norm
         self.sigma = sigma
-        self.last_pull_i = 0
-        self.arm_pull_count = {a_i: self.arm_dim for a_i in range(self.n_arms)}
-        super().__init__(arms, context_set)
 
-    def reset(self):
-        super().reset()
+        self.last_pull_i = 0
+        self.n_pulls = 0
+        self.first = True
+        self.arm_pull_count = {a_i: self.arm_dim for a_i in range(self.n_arms)}
+
         self.V_t = self.lmbd * np.eye(self.arm_dim)
+        self.V_t_inv = np.linalg.inv(self.V_t)
         self.b_vect = np.zeros((self.arm_dim, 1))
         self.theta_hat = np.zeros((self.arm_dim, 1))
         self.last_ucb = np.zeros(self.n_arms)
-        self.first = True
-        self.last_pull_i = 0
-        self.arm_pull_count = {
-            a_i: self.arm_dim for a_i in range(self.n_arms)}
-        return self
 
     def pull_arm(self, context_i=None):
         if self.first:
@@ -125,14 +114,15 @@ class LinUCBAgent(Agent):
         self.a_hist.append(self.last_pull_i)
         return self.last_pull_i
 
-    def update(self, reward, arm_i=None):
+    def update(self, reward, arm_i=None, context_i=None):
+        self.n_pulls += 1
         if arm_i is not None:
             self.last_pull_i = arm_i
         last_pull = self.arms[self.last_pull_i].reshape(self.arm_dim, 1)
         self.V_t = self.V_t + (last_pull @ last_pull.T)
         self.b_vect = self.b_vect + last_pull * reward
-        self.theta_hat = np.linalg.inv(self.V_t) @ self.b_vect
-        self.t += 1
+        self.V_t_inv = np.linalg.inv(self.V_t)
+        self.theta_hat = self.V_t_inv @ self.b_vect
 
     def _estimate_linucb_arm(self):
         bound = self._beta_t_fun_linucb()
@@ -166,33 +156,33 @@ class LinUCBAgent(Agent):
 class ContextualLinUCBAgent(LinUCBAgent):
     def __init__(self, arms, context_set, psi, horizon, lmbd,
                  max_theta_norm_sum, max_arm_norm, sigma=1):
-        self.psi = psi
         super().__init__(arms, context_set, horizon, lmbd,
                          max_theta_norm_sum, max_arm_norm, sigma)
+        self.psi = psi
 
     def pull_arm(self, context_i):
-        self.last_context = self.context_set[context_i]
         if self.first:
             self._pull_round_robin()
         else:
-            self.last_pull_i = self._estimate_linucb_arm()
+            self.last_pull_i = self._estimate_linucb_arm(context_i)
         self.a_hist.append(self.last_pull_i)
         return self.last_pull_i
 
-    def update(self, reward, arm_i=None):
+    def update(self, reward, arm_i=None, context_i=None):
         if arm_i is not None:
             self.last_pull_i = arm_i
-        last_psi = self.psi(self.arms[self.last_pull_i], self.last_context)
+
+        last_psi = self.psi(
+            self.arms[self.last_pull_i], self.context_set[context_i])
         last_psi = last_psi.reshape(self.arm_dim, 1)
         self.V_t = self.V_t + (last_psi @ last_psi.T)
         self.b_vect = self.b_vect + last_psi * reward
         self.theta_hat = np.linalg.inv(self.V_t) @ self.b_vect
-        self.t += 1
 
-    def _estimate_linucb_arm(self):
+    def _estimate_linucb_arm(self, context_i):
         bound = self._beta_t_fun_linucb()
         for i, arm in enumerate(self.arms):
-            psi = self.psi(arm, self.last_context)
+            psi = self.psi(arm, self.context_set[context_i])
             psi = psi.reshape(self.arm_dim, 1)
             self.last_ucb[i] = (self.theta_hat.T @ psi + bound
                                 * np.sqrt(psi.T @ np.linalg.inv(self.V_t) @ psi))
@@ -203,31 +193,26 @@ class INDUCB1Agent(Agent):
     """One independent UCB1Agent instance per context"""
 
     def __init__(self, arms, context_set, max_reward=1):
-        self.max_reward = max_reward
         super().__init__(arms, context_set)
-
-    def reset(self):
-        super().reset()
         self.context_agent = [
             UCB1Agent(
                 self.arms,
                 self.context_set,
-                self.max_reward)
+                max_reward)
             for _ in range(self.n_contexts)
         ]
 
     def pull_arm(self, context_i):
         """arm = argmax (theta * arm + theta_p * arm)"""
-        self.last_context_i = context_i
         arm_i = self.context_agent[context_i].pull_arm()
         self.a_hist.append(arm_i)
         return arm_i
 
-    def update(self, reward, arm_i=None):
+    def update(self, reward, arm_i=None, context_i=None):
         if arm_i is not None:
             self.last_pull_i = arm_i
-        self.context_agent[self.last_context_i].update(
-            reward, self.last_pull_i)
+        self.context_agent[context_i].update(
+            reward, self.last_pull_i, context_i)
 
 
 class INDLinUCBAgent(Agent):
@@ -235,16 +220,12 @@ class INDLinUCBAgent(Agent):
 
     def __init__(self, arms, context_set, horizon, lmbd,
                  max_theta_norm_sum, max_arm_norm, sigma=1):
-        self.context_set = context_set
+        super().__init__(arms, context_set)
         self.lmbd = lmbd
         self.horizon = horizon
         self.max_theta_norm_sum = max_theta_norm_sum
         self.max_arm_norm = max_arm_norm
         self.sigma = sigma
-        super().__init__(arms, context_set)
-
-    def reset(self):
-        super().reset()
         self.context_agent = [
             LinUCBAgent(
                 self.arms,
@@ -259,24 +240,24 @@ class INDLinUCBAgent(Agent):
 
     def pull_arm(self, context_i):
         """arm = argmax (theta * arm + theta_p * arm)"""
-        self.last_context_i = context_i
         arm_i = self.context_agent[context_i].pull_arm()
         self.a_hist.append(arm_i)
         return arm_i
 
-    def update(self, reward, arm_i=None):
+    def update(self, reward, arm_i=None, context_i=None):
         if arm_i is not None:
             self.last_pull_i = arm_i
-        self.context_agent[self.last_context_i].update(
-            reward, self.last_pull_i)
+        self.context_agent[context_i].update(
+            reward, self.last_pull_i, context_i)
 
 
 class ProductContextualAgent(Agent):
-    """Combines a global contextual linear bandit 
+    """Combines a global contextual linear bandit
     and an independent linear bandit per context"""
 
     def __init__(self, arms, context_set, psi, horizon, lmbd, max_theta_norm_shared,
                  max_theta_norm_p, max_arm_norm, sigma):
+        super().__init__(arms, context_set)
         self.lmbd = lmbd
         self.horizon = horizon
         self.max_theta_norm_shared = max_theta_norm_shared
@@ -284,48 +265,13 @@ class ProductContextualAgent(Agent):
         self.max_arm_norm = max_arm_norm
         self.sigma = sigma
         self.psi = psi
-        self.first_context_set = set([i for i in range(len(context_set))])
-        super().__init__(arms, context_set)
 
-    def pull_arm(self, context_i):
-        """arm = argmax (theta * arm + theta_p * arm)"""
-        self.last_context_i = context_i
-        ctx_agent = self.context_agent[context_i]
-        self.agent_global.last_context = self.context_set[context_i]
-
-        if context_i in self.first_context_set:
-            self.last_pull_i, first = ctx_agent._pull_round_robin()
-            if not first:
-                self.first_context_set.remove(context_i)
-        else:
-            # compute and combine their ucb
-            self.agent_global.pull_arm(context_i)
-            ctx_agent.pull_arm()
-
-            ucb = self.agent_global.last_ucb + ctx_agent.last_ucb
-            self.last_pull_i = np.argmax(ucb)
-
-        self.a_hist.append(self.last_pull_i)
-        return self.last_pull_i
-
-    def update(self, reward, arm_i=None):
-        if arm_i is not None:
-            self.last_pull_i = arm_i
-        self.agent_global.update(reward, arm_i=self.last_pull_i)
-        psi = self.psi(self.arms[self.agent_global.last_pull_i],
-                       self.context_set[self.last_context_i])
-        pred_reward = self.agent_global.theta_hat.T @ psi
-        residual = reward - pred_reward
-        self.context_agent[self.last_context_i].update(
-            residual, arm_i=self.last_pull_i)
-
-    def reset(self):
-        super().reset()
         self.first = True
+        self.first_context_set = set([i for i in range(self.n_contexts)])
+
         self.agent_global = ContextualLinUCBAgent(
             self.arms, self.context_set, self.psi, self.horizon, self.lmbd,
             self.max_theta_norm_shared, self.max_arm_norm)
-        self.first_context_set = set([i for i in range(self.n_contexts)])
         self.context_agent = [
             LinUCBAgent(
                 self.arms, self.context_set, self.horizon,
@@ -334,9 +280,41 @@ class ProductContextualAgent(Agent):
             for _ in range(self.n_contexts)
         ]
 
+    def pull_arm(self, context_i):
+        """arm = argmax (theta * arm + theta_p * arm)"""
+        # self.agent_global.last_context = self.context_set[context_i]
+
+        if context_i in self.first_context_set:
+            self.last_pull_i, first = \
+                self.context_agent[context_i]._pull_round_robin()
+            if not first:
+                self.first_context_set.remove(context_i)
+        else:
+            # compute and combine their ucb
+            self.agent_global.pull_arm(context_i)
+            self.context_agent[context_i].pull_arm()
+
+            ucb = self.agent_global.last_ucb + \
+                self.context_agent[context_i].last_ucb
+            self.last_pull_i = np.argmax(ucb)
+
+        self.a_hist.append(self.last_pull_i)
+        return self.last_pull_i
+
+    def update(self, reward, arm_i=None, context_i=None):
+        if arm_i is not None:
+            self.last_pull_i = arm_i
+        self.agent_global.update(reward, arm_i=self.last_pull_i)
+        psi = self.psi(self.arms[self.agent_global.last_pull_i],
+                       self.context_set[context_i])
+        pred_reward = self.agent_global.theta_hat.T @ psi
+        residual = reward - pred_reward
+        self.context_agent[context_i].update(
+            residual, arm_i=self.last_pull_i)
+
 
 class ProductLinearAgent(ProductContextualAgent):
-    """Combines a global linear bandit 
+    """Combines a global linear bandit
     and an independent linear bandit per context"""
 
     def __init__(self, arms, context_set, horizon, lmbd,
